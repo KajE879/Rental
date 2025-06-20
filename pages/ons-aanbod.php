ab3c119b420b93bc438f8e32a2f4664ed53fcb25
KajE879 <9024897@student.zadkine.nl>
2025-06-20 15:45:13 +0200

    remove slider

--- pages/ons-aanbod.php
+++ pages/ons-aanbod.php
@@ -3,31 +3,12 @@
     <div class="container">
         <div class="filter-bar">
             <h3>Filters</h3>
-
             <div class="filter-group">
                 <h4>Type</h4>
                 <label><input type="checkbox" name="category" value="sport"> Sport</label>
                 <label><input type="checkbox" name="category" value="sedan"> Sedan</label>
                 <label><input type="checkbox" name="category" value="suv"> SUV</label>
                 <label><input type="checkbox" name="category" value="hatchback"> Hatchback</label>
-            </div>
-
-            <div class="filter-group">
-            <h4>Fuel Capacity</h4>
-            <input type="range" id="literRange" min="70" max="90" step="10" value="70" />
-            <div id="literValue">70 L</div>
-            </div>
-
-            <div class="filter-group">
-            <h4>Capacity</h4>
-            <input type="range" id="capacityRange" min="2" max="6" step="2" value="2" />
-            <div id="capacityValue">2 Personen</div>
-            </div>
-
-            <div class="filter-group">
-                <h4>Availability</h4>
-                <label><input type="checkbox" name="availability" value="in-stock"> In Stock</label>
-                <label><input type="checkbox" name="availability" value="preorder"> Pre-order</label>
             </div>
         </div>
         <div class="content">
@@ -55,6 +36,5 @@
             </div>
         </div>
     </div>
-<script src="../assets/javascript/slider.js"></script>
 <?php require "../includes/footer.php" ?>
 
